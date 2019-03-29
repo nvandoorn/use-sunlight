e376b52cb760788c8cc34a55d8a971f476d7be00
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-03-29 00:43:38 -0700

    Remove default stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,31 +1,11 @@
 import React from 'react'
 
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
-
-import { Button, Welcome } from '@storybook/react/demo'
 
 import { DemoComponent } from '../src/demo-component'
 
-storiesOf('DemoComponent', module).add('to Storybook', () => (
+storiesOf('DemoComponent', module).add('with heading', () => (
   <DemoComponent>
     <h1 style={{ fontSize: '4em' }}>Hello world</h1>
   </DemoComponent>
 ))
-
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-))
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ))
