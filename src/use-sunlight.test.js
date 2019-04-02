91262718093924a9bd33b5f51f6e0b970077151c
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-04-02 15:48:12 -0700

    (bug) Import react in test

--- src/use-sunlight.test.js
+++ src/use-sunlight.test.js
@@ -1,4 +1,9 @@
 import { computeLightLevel } from './use-sunlight'
+// TODO figure out why this is required
+// (I'm assuming some transpiler magic
+// makes this "required")
+// eslint-disable-next-line
+import React from 'react'
 
 // Test the calculations used in the hook.
 // Not sure if it's worth testing the public hook
